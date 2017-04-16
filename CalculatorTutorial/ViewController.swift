d69adf566dbe730e8dc0ebc92b9e79554dddcd96
dima brodski <dimabrodski@dimas-macbook.local>
2017-04-16 16:09:09 +0300

    Initial Commit

---
+++ CalculatorTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalculatorTutorial
+//
+//  Created by dima brodski on 16/04/2017.
+//  Copyright © 2017 dima brodski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
